18dd6f7e38c0c437494a63e02490020d13b5ae1c
Mikhail Gostev <gostevvm@yandex.ru>
2019-01-10 02:34:49 +0300

    Add project skeleton and small changes
    Add skeleton faces beans
    Create Request class
    Added web and faces-config configurations
    Added train beans into .../train/
    Added trains home page into webapp/
    Add dependencies to javax.servlet-api and postgresql into pom
    Add maven-war-plugin plugin into pom
    Add source directory into build properties in pom

---
+++ src/main/java/ru/bepis/AreaCheckBean.java
@@ -0,0 +1,10 @@
+package ru.bepis;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "helloWorld", eager = true)
+@RequestScoped
+public class AreaCheckBean {
+  // todo implement
+}
---
+++ src/main/java/ru/bepis/PointValidator.java
@@ -0,0 +1,10 @@
+package ru.bepis;
+
+public class PointValidator {
+
+  // todo implement
+  public static boolean isValidPoint(double x, double y, double r) {
+    return false;
+  }
+
+}
