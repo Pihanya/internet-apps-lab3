5aaffc347d9430726c800143c363f483db3ab6a0
Mikhail Gostev <gostevvm@yandex.ru>
2019-01-10 04:48:20 +0300

    Changed AreaCheckBean

--- src/main/java/ru/bepis/bean/AreaCheckBean.java
+++ src/main/java/ru/bepis/bean/AreaCheckBean.java
@@ -1,5 +1,7 @@
 package ru.bepis.bean;
 
+import static ru.bepis.util.ConstantValues.DOUBLE_MACHINE_EPSILON;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
@@ -10,6 +12,9 @@ import ru.bepis.model.Request;
 @RequestScoped
 @Data
 public class AreaCheckBean {
+
+  private static final double EPS = DOUBLE_MACHINE_EPSILON;
+
   @ManagedProperty(value = "#{userRequest}")
   private Request request;
 
@@ -17,8 +22,23 @@ public class AreaCheckBean {
     this.request = new Request(x, y, r, isValidPoint(x, y, r));
   }
 
-  // todo implement
   public static boolean isValidPoint(double x, double y, double r) {
-    return false;
+    if (Math.abs(y) <= EPS) {
+      return r - Math.abs(x) >= 0;
+    }
+
+    if (Math.abs(x) <= EPS) {
+      return r - Math.abs(y) >= 0;
+    }
+
+    if ((x > 0) && (y > 0)) {
+      return (y + 2 * x <= r);
+    } else if ((x > 0) && (y < 0)) {
+      return (Math.sqrt(x * x + y * y) <= r);
+    } else if ((x < 0) && (y > 0)) {
+      return (x >= -r) && (y <= r);
+    } else {
+      return false;
+    }
   }
 }
