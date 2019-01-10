b6952333bb931f7fc883736546570257a5b28cc3
Mikhail Gostev <gostevvm@yandex.ru>
2019-01-10 05:06:15 +0300

    Correct methods in areaCheck bean

--- src/main/java/ru/bepis/bean/AreaCheckBean.java
+++ src/main/java/ru/bepis/bean/AreaCheckBean.java
@@ -14,15 +14,15 @@ public class AreaCheckBean {
   private static final double EPS = DOUBLE_MACHINE_EPSILON;
 
   @ManagedProperty(value = "#{x}")
-  private double x;
+  private Double x;
 
   @ManagedProperty(value = "#{y}")
-  private double y;
+  private Double y;
 
   @ManagedProperty(value = "#{r}")
-  private double r;
+  private Double r;
 
-  public boolean isValidPoint(double x, double y, double r) {
+  public static boolean validate(double x, double y, double r) {
     if (Math.abs(y) <= EPS) {
       return r - Math.abs(x) >= 0;
     }
@@ -40,5 +40,9 @@ public class AreaCheckBean {
     } else {
       return false;
     }
+  }
+
+  public boolean isValidPoint() {
+    return validate(x, y, r);
   }
 }
